c288e2cfd99fa0828e0af1083ea93e6599e500cb
contactharrisc2 <contactharrisc2@gmail.com>
2022-04-10 12:16:42 -0500

    adds change to body background-color according to the value of variable bankAccount  in index.ejs. Wasn't sure how to setup DOM to make changes to node elements, so I hard coded the feature

--- server.js
+++ server.js
@@ -4,41 +4,74 @@ require("dotenv").config();
 ////////////////////////
 
 const budget = require("./models/budget.js");
+const bankAccount = () =>{
+    let sum = 0;
+    for(let i = 0; i < budget.length; i++){
+        sum += budget[i].amount
+    }
+    
+    return sum
+}
+
+console.log(bankAccount())
 
 
 ////////////////////////
 // Setup - Import deps and create app object
 ////////////////////////
 const expressServer = require("express");
+const { redirect } = require("express/lib/response");
 const app = expressServer();
 const PORT = process.env.PORT;
 
 //////////////////////
 // Declare Middleware
 //////////////////////
-app.use(expressServer.static('views'))
-// app.use('/static',expressServer.static('public'))
+// app.use(expressServer.static('public'))
+app.use('/static',expressServer.static('public'))
 app.use(expressServer.urlencoded({extended: false})); //need this because html form data is not JSON formatted
 
 ///////////////////////
 // Declare Routes and Routers 
 ///////////////////////
 
-//root/////
+//root
 app.get("/", (req, res) => {
     res.send("You are at the root");
 });
 
 //index
-app.get("/budgtr", (req, res) => {
-    
-    res.render("index.ejs",{budget: budget},)
+app.get("/budgtr/", (req, res) => {
+    console.log("Account: ", bankAccount())
+    res.render("index.ejs",{
+        budget: budget,
+        bankAccount: bankAccount(),
+    })
+});
+
+//new
+app.get("/budgtr/new/", (req, res) =>{
+    res.render("new.ejs",{an_expense: budget[0]});
+
+});
+
+app.post("/budgtr/",(req, res)=>{
+    // console.log(req.body);
+    const tagsArray = req.body.tags.split(",");
+    req.body.tags = null;
+    req.body.tags = [...tagsArray];
+    req.body.amount = parseFloat(req.body.amount);
+    budget.push(req.body);
+    // console.log("budget: ",budget);
+    res.redirect("/budgtr/");
 });
 
 //show
 app.get("/budgtr/:id", (req, res)=>{
-    res.render("show.ejs",{expenditure:budget[req.params.id]},)
+    res.render("show.ejs",{expenditure:budget[req.params.id]})
 });
+
+
 
 ///////////////////////////
 // Server Listener
